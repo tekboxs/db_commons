afdda02bf89c27a6c923802120795c9854fc84ba
Tekboxs <tekboxs@gmail.com>
2023-06-07 09:53:59 -0400

    export fix

--- lib/db_commons.dart
+++ lib/db_commons.dart
@@ -1,5 +1,5 @@
 library meal_data_base;
 
 export 'src/db_commons.dart';
-export 'src/domain/service/hive_configurator_service.dart';
+export 'src/domain/service/hive_simple_service.dart';
 export 'src/domain/service/hive_custom_service.dart';
