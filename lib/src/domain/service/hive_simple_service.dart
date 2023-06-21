629984d117087f7cb013c6263fd7f937d518bfad
Tekboxs <tekboxs@gmail.com>
2023-06-21 11:25:09 -0400

    small fix

--- lib/src/domain/service/hive_simple_service.dart
+++ lib/src/domain/service/hive_simple_service.dart
@@ -3,33 +3,35 @@ import 'package:hive/hive.dart';
 ///Save simple data like primitives types
 class HiveSimpleService {
   final String boxName;
-  late Box _box;
+  Box? box;
 
   HiveSimpleService({required this.boxName});
 
   ///open box to make possible read and update
   _init() async {
-    _box = await Hive.openBox(boxName);
+    if (box == null || !box!.isOpen) {
+      box = await Hive.openBox(boxName);
+    }
   }
 
   ///return if a key already in memory
   ///can be used to handle update or add
   Future<bool> existKey(dynamic key) async {
     await _init();
-    return _box.containsKey(key.toString());
+    return box!.containsKey(key.toString());
   }
 
   ///remove all current data
   Future<void> clear() async {
     await _init();
-    await _box.clear();
+    await box!.clear();
   }
 
   ///remove only a key
   ///have no effect if this doesnt exists
   Future deleteMethod(key) async {
     await _init();
-    return await _box.delete(key.toString());
+    return await box!.delete(key.toString());
   }
 
   ///return stored value this service only handle
@@ -37,9 +39,9 @@ class HiveSimpleService {
   Future<dynamic> readMethod(dynamic key) async {
     await _init();
     if (key is int) {
-      return await _box.getAt(key);
+      return await box!.getAt(key);
     } else {
-      return await _box.get(key.toString());
+      return await box!.get(key.toString());
     }
   }
 
@@ -48,18 +50,18 @@ class HiveSimpleService {
   Future<void> writeMethod(dynamic key, dynamic value) async {
     await _init();
     if (key is int) {
-      await _box.putAt(key, value);
+      await box!.putAt(key, value);
     }
-    await _box.put(key.toString(), value);
+    await box!.put(key.toString(), value);
   }
 
   Future<int> get memoryLength async {
     await _init();
-    return _box.length;
+    return box!.length;
   }
 
   Future<List<dynamic>> get getAllItens async {
     await _init();
-    return [for (var key in _box.keys) await readMethod(key)];
+    return [for (var key in box!.keys) await readMethod(key)];
   }
 }
